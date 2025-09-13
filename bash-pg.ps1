6491cf882cefe01922b4815c93c290cfb00ebeeb
Stuart Williams <spookdejur@hotmail.com>
2025-09-13 10:35:44 -0700

    WIP,6

--- bash-pg.ps1
+++ bash-pg.ps1
@@ -13,8 +13,8 @@
         Sucess or failure 
 #>
 [string]$NAME='postgressvr'
-[string]$VOL='/var/lib/postgresql/data/salesdb'
+[string]$VOL='/var/lib/postgresql/data'
 [string]$BIN='/usr/lib/postgresql/17/bin'
 Write-Output "SQL Scripts Folder: ${VOL}"
 Write-Output "Postgres Utilities Folder: ${BIN}"
-docker exec -it --workdir "${VOL}"  "${NAME}" /bin/bash
+docker exec -it --workdir "${VOL}" "${NAME}" /bin/bash
