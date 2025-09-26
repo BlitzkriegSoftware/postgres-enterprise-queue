ff9f819d66eb02b9db55bc101fdc616966f85cd5
Stuart Williams <spookdejur@hotmail.com>
2025-09-26 16:52:47 -0700

    WIP,oh

--- data/pg_cron_add.sh
+++ data/pg_cron_add.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+export username="postgres"
+export PGPASSWORD="password123-"
+export master="postgres"
 set -o xtrace
 cd /usr/lib/postgresql/16/bin
+./psql  -h "localhost" -U "${username}" -d "${master}" -a -f /var/lib/postgresql/data/sql/020_PG_CRON_EXT.sql
 set +o xtrace
