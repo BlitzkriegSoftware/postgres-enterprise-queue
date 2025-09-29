98eb465bd719f12345678e999a422a414bfc0d63
Stuart Williams <spookdejur@hotmail.com>
2025-09-28 17:01:41 -0700

    WIP,u,aa

--- data/pg_cron_add.sh
+++ data/pg_cron_add.sh
@@ -4,5 +4,5 @@ export PGPASSWORD="password123-"
 export master="postgres"
 set -o xtrace
 cd /usr/lib/postgresql/16/bin
-./psql  -h "localhost" -U "${username}" -d "${master}" -a -f /var/lib/postgresql/data/sql/020_PG_CRON_EXT.sql
+./psql  -h "localhost" -U "${username}" -d "${master}" -a -f /var/lib/postgresql/data/sql/020_create_extensions.sql
 set +o xtrace
