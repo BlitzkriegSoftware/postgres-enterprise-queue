c17bddff5d89557867a6790b5fdbbfa934927e4e
Stuart Williams <spookdejur@hotmail.com>
2025-10-07 20:15:20 -0700

    WIP,u,76

--- data/util/test-que-de.sql
+++ data/util/test-que-de.sql
@@ -12,7 +12,7 @@ BEGIN
 
 	select b.msg_id, b.expires, b.msg_json 
 	into msg_id, expires, msg_json 
-	from test01.dequeue('client03', 60) as b;
+	from test01.dequeue('client03', 30) as b;
 	
 	RAISE NOTICE 'id: %, expires: %, json: %', msg_id, expires, msg_json;
 	
