f0983ab373cf3ff2553a78106fe722682e065393
Stuart Williams <spookdejur@hotmail.com>
2025-10-07 20:13:09 -0700

    WIP,u,75

--- data/util/test-que-de.sql
+++ data/util/test-que-de.sql
@@ -4,12 +4,12 @@
 DO $$
 
 DECLARE 
-	textr varchar;
 	msg_id varchar;
 	expires TIMESTAMP;
 	msg_json json;
-	a text[];
+
 BEGIN
+
 	select b.msg_id, b.expires, b.msg_json 
 	into msg_id, expires, msg_json 
 	from test01.dequeue('client03', 60) as b;
