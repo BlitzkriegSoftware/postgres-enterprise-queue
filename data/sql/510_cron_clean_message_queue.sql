d7e8aa69f80104e6338429310b26ae5c7d2b8611
Stuart Williams <spookdejur@hotmail.com>
2025-10-10 19:41:09 -0700

    v1, rc2, e

---
+++ data/sql/510_cron_clean_message_queue.sql
@@ -0,0 +1,46 @@
+DROP PROCEDURE IF EXISTS {schema}.cron_clean_message_queue();
+
+CREATE OR REPLACE PROCEDURE {schema}.cron_clean_message_queue(
+    IN max_retries integer DEFAULT 7,
+    IN ts_exeeded TIMESTAMP := CURRENT_TIMESTAMP
+)
+
+LANGUAGE plpgsql
+AS $BODY$
+
+DECLARE
+        dead_cursor CURSOR FOR 
+            select message_id 
+                from {schema}.message_queue 
+                where 
+                    (
+                        (retries > max_retries)
+                        OR
+                        (created_on < ts_exeeded)
+                    )
+                    ;
+BEGIN
+
+    OPEN dead_cursor;
+
+    LOOP
+        FETCH dead_cursor INTO msg_id; 
+        EXIT WHEN NOT FOUND; 
+
+        INSERT INTO {schema}.dead_letter(message_id, message_state_id, created_on, dead_on, created_by, reason_why, message_json)
+            SELECT message_id, state_id, created_on, CURRENT_TIMESTAMP, who_by, why_so, message_json
+            FROM {schema}.message_queue  
+            WHERE message_id = msg_id;
+
+        DELETE from {schema}.message_queue WHERE message_id = msg_id;
+
+        call {schema}.add_audit(msg_id, state_id, who_by, why_so);
+    END LOOP;
+
+    CLOSE dead_cursor;
+   
+END;
+$BODY$;
+
+ALTER PROCEDURE {schema}.cron_clean_message_queue( integer, TIMESTAMP )
+    OWNER TO postgres;
--- data/sql/520_cron_unlock.sql
+++ data/sql/520_cron_unlock.sql
@@ -7,30 +7,24 @@ LANGUAGE plpgsql
 AS $BODY$
 
 DECLARE
-    ts TIMESTAMP;
+    item_ttl integer := 0;
+    item_ttl_default integer := 4320;
     lease_duration integer := 0;
     lease_duration_default integer := 30;
     max_retries integer := 0;
     max_retries_default integer := 7;
-    item_ttl integer := 0;
-    item_ttl_default integer := 4320;
     msg_id uuid;
     state_id integer := 88;
+    ts TIMESTAMP;
+    ts_exeeded TIMESTAMP := CURRENT_TIMESTAMP;
     who_by varchar(128) := 'system';
     why_so varchar(128) := 'retries or TTL exeeded';
-    ts_exeeded TIMESTAMP := CURRENT_TIMESTAMP;
-    
     
 BEGIN
     select COALESCE(CAST(setting_value AS INTEGER), max_retries_default)
         into max_retries
         from {schema}.queue_configuration 
         where setting_name = 'max_retries';
-
-    select COALESCE(CAST(setting_value AS INTEGER), item_ttl_default)
-        into item_ttl
-        from {schema}.queue_configuration 
-        where setting_name = 'item_ttl';
 
     lease_duration := lease_duration_in;
     if lease_duration <= 0 then
@@ -58,39 +52,16 @@ BEGIN
         AND
         lease_expires < ts;
 
-    -- move really dead one to dead letter
+    --
+    -- clean out message queue
+    select COALESCE(CAST(setting_value AS INTEGER), item_ttl_default)
+        into item_ttl
+        from {schema}.queue_configuration 
+        where setting_name = 'item_ttl';
     -- years, months, weeks, days, hours, mins, secs
     ts_exeeded := CURRENT_TIMESTAMP -  make_interval( 0, 0, 0, 0, 0, item_ttl, 0 );
-
-    DECLARE
-        dead_cursor CURSOR FOR 
-        select message_id 
-            from {schema}.message_queue 
-            where 
-                (
-                (retries > max_retries)
-                OR
-                (created_on < ts_exeeded)
-                )
-                ;
-
-    OPEN dead_cursor;
-
-    LOOP
-        FETCH dead_cursor INTO msg_id; 
-        EXIT WHEN NOT FOUND; 
-
-        INSERT INTO {schema}.dead_letter(message_id, message_state_id, created_on, dead_on, created_by, reason_why, message_json)
-            SELECT message_id, state_id, created_on, CURRENT_TIMESTAMP, who_by, why_so, message_json
-            FROM {schema}.message_queue  
-            WHERE message_id = msg_id;
-
-        DELETE from {schema}.message_queue WHERE message_id = msg_id;
-
-        call {schema}.add_audit(msg_id, state_id, who_by, why_so);
-    END LOOP;
-
-    CLOSE dead_cursor;
+    -- move really dead ones to dead letter
+    CALL {schema}.cron_clean_message_queue( max_retries, ts_exeeded );
 
 END;
 $BODY$;
