6a01a971ee239bc5aa3b2c732376821c84c59ebc
Stuart Williams <spookdejur@hotmail.com>
2025-10-10 19:28:43 -0700

    v1, rc2, d

--- data/sql/502_cron_unlock.sql
+++ data/sql/502_cron_unlock.sql
@@ -12,17 +12,25 @@ DECLARE
     lease_duration_default integer := 30;
     max_retries integer := 0;
     max_retries_default integer := 7;
+    item_ttl integer := 0;
+    item_ttl_default integer := 4320;
     msg_id uuid;
     state_id integer := 88;
     who_by varchar(128) := 'system';
-    why_so varchar(128) := 'retries exeeded';
-    dead_cursor CURSOR FOR select message_id from {schema}.message_queue where retries > max_retries;
+    why_so varchar(128) := 'retries or TTL exeeded';
+    ts_exeeded TIMESTAMP := CURRENT_TIMESTAMP;
+    
     
 BEGIN
-    select COALESCE(CAST(max_retries AS INTEGER), max_retries_default)
+    select COALESCE(CAST(setting_value AS INTEGER), max_retries_default)
         into max_retries
         from {schema}.queue_configuration 
         where setting_name = 'max_retries';
+
+    select COALESCE(CAST(setting_value AS INTEGER), item_ttl_default)
+        into item_ttl
+        from {schema}.queue_configuration 
+        where setting_name = 'item_ttl';
 
     lease_duration := lease_duration_in;
     if lease_duration <= 0 then
@@ -51,7 +59,20 @@ BEGIN
         lease_expires < ts;
 
     -- move really dead one to dead letter
-   
+    -- years, months, weeks, days, hours, mins, secs
+    ts_exeeded := CURRENT_TIMESTAMP -  make_interval( 0, 0, 0, 0, 0, item_ttl, 0 );
+
+    DECLARE
+        dead_cursor CURSOR FOR 
+        select message_id 
+            from {schema}.message_queue 
+            where 
+                (
+                (retries > max_retries)
+                OR
+                (created_on < ts_exeeded)
+                )
+                ;
 
     OPEN dead_cursor;
 
